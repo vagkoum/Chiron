53232e93643ae5103d35ea934e2a723126d311df
vagkoum <vagkoum4290@gmail.com>
2026-08-10 15:01:16 +0300

    Import TrustBadge component in Admin.js

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,3 +1,4 @@
+import { TrustBadge } from '../components/TrustBadge'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
