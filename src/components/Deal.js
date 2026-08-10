20a1217507fb60e373922458f63e25a861033c70
vagkoum <vagkoum4290@gmail.com>
2026-08-10 09:56:26 +0300

    Import trust score functions in Deal component

--- src/components/Deal.js
+++ src/components/Deal.js
@@ -1,3 +1,4 @@
+import { canStartDeal, updateTrustScore } from '../lib/trustScore'
 import { useState, useEffect } from 'react'
 import { supabase } from '../lib/supabase'
 import { useAuth } from '../lib/AuthContext'
