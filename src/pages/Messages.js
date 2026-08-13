977efc4713e414aa2b17c928edfede9464b04b28
vagkoum <vagkoum4290@gmail.com>
2026-08-13 14:17:47 +0300

    Import AccessRequestPanel in Messages component

--- src/pages/Messages.js
+++ src/pages/Messages.js
@@ -3,6 +3,7 @@ import { useParams, useNavigate } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
 import { useAuth } from '../lib/AuthContext'
 import { DealPanel } from '../components/Deal'
+import { AccessRequestPanel } from '../components/NDA'
 
 export default function Messages() {
   const { user } = useAuth()
