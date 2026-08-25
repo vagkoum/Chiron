2148076a2bf6adfae8e7028306bb1434cf0fed1d
vagkoum <vagkoum4290@gmail.com>
2026-08-25 13:21:47 +0300

    Import useNavigate from react-router-dom

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import { supabase } from '../lib/supabase'
 import { useAuth } from '../lib/AuthContext'
 import { canLeaveReview, updateTrustScore } from '../lib/trustScore'
