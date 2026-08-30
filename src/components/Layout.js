9d3ace4db995235e3a1d3cdaf81743e5c8d8632c
vagkoum <vagkoum4290@gmail.com>
2026-08-30 17:56:38 +0300

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -116,10 +116,6 @@ export default function Layout() {
             <span>Browse</span>
           </NavLink>
 
-          <NavLink to="/how-it-works" className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')} title="Learn how Chiron works">
-            <span>How it works</span>
-          </NavLink>
-
           {user && (
             <>
               <NavLink to="/matches" className={({ isActive }) => 'nav-link' + (isActive ? ' active' : '')} title="See suggested matches for your submissions">
