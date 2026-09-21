1fb263a7acc1718baf8512687f53d51687427fb2
vagkoum <vagkoum4290@gmail.com>
2026-09-21 10:41:42 +0300

    Update terms version and effective date

--- src/lib/legalConfig.js
+++ src/lib/legalConfig.js
@@ -1,2 +1,2 @@
-export const CURRENT_TERMS_VERSION = 'v1'
-export const TERMS_EFFECTIVE_DATE = '2026-09-18'
+export const CURRENT_TERMS_VERSION = 'v2'
+export const TERMS_EFFECTIVE_DATE = '2026-09-21'
