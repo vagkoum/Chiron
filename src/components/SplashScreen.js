1ba23730c3530c011f4bc990f9a9205416be43bf
vagkoum <vagkoum4290@gmail.com>
2026-09-22 21:43:13 +0300

    Increase splash screen timeout from 2200ms to 2450ms

--- src/components/SplashScreen.js
+++ src/components/SplashScreen.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 export default function SplashScreen({ onDone }) {
 
   useEffect(() => {
-    const doneTimer = setTimeout(() => onDone(), 2200)
+    const doneTimer = setTimeout(() => onDone(), 2450)
     return () => {
       clearTimeout(doneTimer)
     }
